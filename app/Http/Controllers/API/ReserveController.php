63abf285fdf7e0b68371033967a4250c4a379b7d
Fredys Muriel <72772732+FredysMuriel4@users.noreply.github.com>
2022-08-08 23:09:28 -0500

    Reference Lesson model, done

--- app/Http/Controllers/API/ReserveController.php
+++ app/Http/Controllers/API/ReserveController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Reserve;
+use App\Models\Lesson;
 use Auth;
 use Illuminate\Support\Facades\Validator;
 
